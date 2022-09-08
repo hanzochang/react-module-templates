fcafac9645cec4a834fcd7619d4c4099d3879f8a
hanzochang <hanzo.chan@gmail.com>
2022-09-08 19:30:50 +0900

    Setup lint tools

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-no-target-blank': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@next/next/no-img-element': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['src/generated/*.tsx'],
+}
