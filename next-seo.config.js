613dbc3e161d2c7251018b63c99fb98bb38b3b7a
hanzochang <hanzo.chan@gmail.com>
2022-09-08 19:46:02 +0900

    Setup seo

---
+++ next-seo.config.js
@@ -0,0 +1,35 @@
+const config = {
+  title: '',
+  description: '',
+  openGraph: {
+    url: '',
+    title: '',
+    description: '',
+    images: [
+      {
+        url: '',
+        width: 2400,
+        height: 1260,
+        alt: '',
+      },
+    ],
+    siteName: '',
+  },
+  twitter: {
+    handle: '@awesome',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+  additionalMetaTags: [
+    {
+      name: 'keywords',
+      content: '',
+    },
+    {
+      name: 'theme-color',
+      content: '#999999',
+    },
+  ],
+}
+
+export default config
